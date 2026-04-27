e7255315d5aa0907c44529fb8237284aa7fbba82
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-27 15:25:18 +0700

    Menambahkan mahasiswa joko

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -7,4 +7,15 @@ private:
     string nama;
 public:
     friend void setNama(mahasiswa &a, string)
+};
+
+void setNama(mahasiswa 8a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
 }
