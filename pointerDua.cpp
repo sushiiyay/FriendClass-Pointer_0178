071847fb492d97984e152829e778f975cc16880e
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-27 15:10:32 +0700

    Menambahkan pointer object mhs

---
+++ pointerDua.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    int nim;
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // pointer Object mhs
+}
--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -9,5 +9,15 @@ class mahasiswa{
 };
 
 int main(){
-    
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointer Derefence pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
 }
