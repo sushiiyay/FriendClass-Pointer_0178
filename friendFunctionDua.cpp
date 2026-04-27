5f82dce2be8e6741aaafac1c3e536e0f4dbc8974
sushiiyay <nfa.aliya7811@gmail.com>
2026-04-27 15:39:12 +0700

    Menambahkan manusia budi dan pelajar pbudi

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -3,3 +3,26 @@ using namespace std;
 
 class pelajar;
 class manusia
+{
+public:
+    void showNilaiPelajar(pelajar &x);
+};
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
+
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+int main()
+{
+    manusia budi;
+}
